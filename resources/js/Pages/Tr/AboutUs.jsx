d1c4ecb3d966293125dd48c81075ad5d24ddd2eb
SanazBarzgar <sbarzegar95@gmail.com>
2024-04-09 18:28:52 +0330

    b

--- resources/js/Pages/Tr/AboutUs.jsx
+++ resources/js/Pages/Tr/AboutUs.jsx
@@ -12,14 +12,7 @@ const AboutUs = () => {
             </Head>
             <WebSiteTRLayout>
                 <div className="flex flex-row text-3xl text-white" >Hakkimizda:
-
-                <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, facere nesciunt. Minima, corrupti laboriosam eligendi voluptas non illo aspernatur fugiat nam ad sint, ullam autem amet quisquam labore soluta magni.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis at harum officiis et aspernatur dolorum nemo perspiciatis ipsum veritatis accusantium, temporibus saepe adipisci excepturi quibusdam a autem, quos vitae non!
-                </p>
-                
                 </div>
             </WebSiteTRLayout>
         </>
