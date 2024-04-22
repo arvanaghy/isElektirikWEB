8af0b5e70220c98da63bd55ce33af7fb8510ed3d
SanazBarzgar <sbarzegar95@gmail.com>
2024-04-22 19:08:09 +0330

    En Phonecall component

--- resources/js/Components/WebSiteENLayout.jsx
+++ resources/js/Components/WebSiteENLayout.jsx
@@ -3,6 +3,8 @@ import NavBar from "@/Components/NavBar";
 import TopBar from "@/Components/TopBar";
 import Footer from "@/Components/Footer";
 import SideScroll from "./SideScroll";
+import BackToTopButton from "./BackToTopButton";
+import PhoneCallButtonEN from "./PhoneCallButtonEN";
 const WebSiteENLayout = ({ children }) => {
     return (
         <>
@@ -10,6 +12,8 @@ const WebSiteENLayout = ({ children }) => {
             <NavBar />
             <SideScroll />
             <main className="min-h-screen">{children}</main>
+            <BackToTopButton />
+            <PhoneCallButtonEN />
             <Footer />
         </>
     );
