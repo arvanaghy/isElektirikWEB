de08eadd046ae119cba4d59f5eb46bbb07bc4fda
arvanaghy <arvanaghy@gmail.com>
2024-04-23 10:57:32 -0700

    سیب

--- resources/js/Pages/ECatalog.jsx
+++ resources/js/Pages/ECatalog.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef }  from "react";
+import React, { useEffect, useRef } from "react";
 import WebSiteENLayout from "@/Components/WebSiteENLayout";
 import { Head, Link } from "@inertiajs/react";
 
@@ -6,7 +6,7 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
 
-const ECatalog = ({ 
+const ECatalog = ({
     catalog_list,
     lastPage,
     currentPage,
@@ -46,12 +46,12 @@ const ECatalog = ({
                 <meta name="author" content="" />
             </Head>
             <WebSiteENLayout address={address?.general_value} phone={phone?.general_value} email={email?.general_value} telegram={telegram?.general_value} linkdin={linkdin?.general_value} insta={insta?.general_value} about_en={about_us_text_en?.general_value}>
-            <div className="flex flex-col bg-[url('../images/contact-us.jpg')] py-16 text-center bg-cover">
-                        <h5 ref={eCatalog}  className="lg:text-6xl text-4xl  mt-16 lg:mt-0">
-                            <span className="inline-block lg:first-letter:text-7xl first-letter:text-5xl first-letter:text-green-500">E-</span>
-                            <span className="inline-block lg:first-letter:text-7xl first-letter:text-5xl first-letter:text-green-500">Catalog</span>
-                        </h5>
-                    </div>
+                <div className="flex flex-col bg-[url('../images/contact-us.jpg')] py-16 text-center bg-cover">
+                    <h5 ref={eCatalog} className="lg:text-6xl text-4xl  mt-16 lg:mt-0">
+                        <span className="inline-block lg:first-letter:text-7xl first-letter:text-5xl first-letter:text-green-500">E-</span>
+                        <span className="inline-block lg:first-letter:text-7xl first-letter:text-5xl first-letter:text-green-500">Catalog</span>
+                    </h5>
+                </div>
                 <div className=" flex flex-col items-center justify-center px-10 py-20 ">
                     {catalog_list.data.length < 1 && (
                         <div className="flex flex-col items-center justify-center bg-white py-16 px-16" >
@@ -64,35 +64,30 @@ const ECatalog = ({
                         {
                             catalog_list.data.map((item, index) => (
                                 <div key={index} className="lg:text-xl border flex flex-col lg:flex-row lg:justify-between lg:px-20 lg:items-center rounded-md py-8 shadow-lg" >
-                                    <p  className=" border-l-4 flex flex-row justify-between lg:justify-normal px-4 border-green-400" >
+                                    <p className=" border-l-4 flex flex-row justify-between lg:justify-normal px-4 border-green-400" >
                                         Catalog Title: <span className="text-gray-500 lg:pl-10">{item.title}</span>
                                     </p>
                                     <p className=" border-l-4 flex flex-col lg:flex-row lg:items-center pt-4 lg:pt-0 justify-between lg:justify-normal px-4 border-green-400" >
                                         Catalog description: <span className="pt-2 lg:pt-0 text-gray-500 lg:pl-10">{item.description}</span>
                                     </p>
-
-                                    
-                                    <Link href={item.url} className=" hover:text-green-800 h-full my-auto pt-5 lg:pt-0 transition-all duration-300  text-green-500" > Download </Link>
-
+                                    <a target="_blank" href={`../../catalog/${item.download_filename}`} download={item.download_filename} className=" hover:text-green-800 h-full my-auto pt-5 lg:pt-0 transition-all duration-300  text-green-500" > Download </a>
                                 </div>
-                                
                             )
                             )
                         }
-
                     </div>
                     {lastPage > 1 && (
                         <div className="flex flex-col items-center w-full py-3">
                             <div className="flex flex-row w-full h-32 items-center justify-center ">
-                                <Link href="/projects?page=1" className="hover:text-green-800 transition-all duration-300  text-green-500">
+                                <Link href="/e-catalog?page=1" className="mx-1 hover:text-green-800 transition-all duration-300  text-green-500">
                                     FitrstPage
                                 </Link>
                                 {range.map((page) => (
                                     <Link
-                                        href={`/projects?page=${page}`}
+                                        href={`/e-catalog?page=${page}`}
                                         className={`mx-2 border-2 border-green-400 p-2 rounded-md hover:text-green-800 transition-all duration-300 text-green-500 ${page === currentPage ? "bg-green-500 text-white" : ""} `}>{page}</Link>
                                 ))}
-                                <Link href={`/projects?page=${lastPage}`} className="hover:text-green-800 transition-all duration-300 text-green-500">
+                                <Link href={`/e-catalog?page=${lastPage}`} className="mx-1 hover:text-green-800 transition-all duration-300 text-green-500">
                                     lastPage
                                 </Link>
                             </div>
