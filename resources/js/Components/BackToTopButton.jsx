8a7a267e3a8d56e5f31a6b28fce32fd82adf166b
arvanaghy <arvanaghy@gmail.com>
2024-04-23 06:29:39 -0700

    asd

--- resources/js/Components/BackToTopButton.jsx
+++ resources/js/Components/BackToTopButton.jsx
@@ -29,6 +29,7 @@ const BackToTopButton = () => {
             className={`fixed cursor-pointer hover:opacity-70 opacity-50  bottom-4 right-4 m-w-10 m-h-10 bg-black shadow shadow-black rounded-xl  transition-all duration-300 delay-150 ${
                 scrollPosition > 200 ? "opacity-100" : "opacity-0"
             } `}
+            style={{ zIndex: "9999" }}
         >
             <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18.2929 15.2893C18.6834 14.8988 18.6834 14.2656 18.2929 13.8751L13.4007 8.98766C12.6195 8.20726 11.3537 8.20757 10.5729 8.98835L5.68257 13.8787C5.29205 14.2692 5.29205 14.9024 5.68257 15.2929C6.0731 15.6835 6.70626 15.6835 7.09679 15.2929L11.2824 11.1073C11.673 10.7168 12.3061 10.7168 12.6966 11.1073L16.8787 15.2893C17.2692 15.6798 17.9024 15.6798 18.2929 15.2893Z" fill="#ffffff"></path> </g></svg>
         </div>
